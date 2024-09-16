9013b07452281c596de81684b5a339772889905e
giangdangtuan <giangbe291@gmail.com>
2024-09-16 14:04:13 +0700

    cải thiện lần 1

--- task2/Bai2_SoNguyenTo.java
+++ task2/Bai2_SoNguyenTo.java
@@ -7,7 +7,7 @@ public class Bai2_SoNguyenTo {
       if(n<2){
           return false;
       }
-      for(int i=2;i<= Math.sqrt(n)/2;i++){
+      for(int i=2;i <= Math.sqrt(n)/2;i++){
           if(n%i==0){
               return false;
           }
