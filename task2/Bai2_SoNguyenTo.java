df3b06570870e14eca491f4d55354c3bc0a69e93
giangdangtuan <giangbe291@gmail.com>
2024-09-16 14:05:44 +0700

    cải thiện lần 1

--- task2/Bai2_SoNguyenTo.java
+++ task2/Bai2_SoNguyenTo.java
@@ -7,7 +7,7 @@ public class Bai2_SoNguyenTo {
       if(n<2){
           return false;
       }
-      for(int i=2;i <= Math.sqrt(n)/2;i++){
+      for(int i=2 ;i <= Math.sqrt(n)/2;i++){
           if(n%i==0){
               return false;
           }
@@ -23,7 +23,7 @@ public class Bai2_SoNguyenTo {
         if(KTSNT(n)){
             System.out.println("N là số nguyên tố !");
         } else{
-            System.out.println("N không phải số nguyên tố");
+            System.out.println("N không phải số nguyên tố !");
         }
     }
 }
